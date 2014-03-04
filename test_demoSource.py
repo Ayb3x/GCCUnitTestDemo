1aa86a7533bf5627e637a61c9e951275f722843f
Erica Pisani <pisani.erica@gmail.com>
2014-03-04 09:15:20 -0500

    Add a couple more tests.

--- test_demoSource.py
+++ test_demoSource.py
@@ -25,10 +25,16 @@ class TestDemoSource(unittest.TestCase):
 		result = src.multiply(self.numA, self.numB)
 		self.assertEqual(result, 20)
 
+	def test_multiplyNegativeNumbers(self):
+		result = src.multiply(-1, self.numA)
+		self.assertEqual(result, -4)
+
 	def test_divideNumbers(self):
 		result = src.divide(self.numB, self.numA)
 		self.assertEqual(result, 1)
 
+	def test_divideByZero(self):
+		self.assertRaises(ZeroDivisionError, src.divide, self.numA, 0)
 
 if __name__ == '__main__':
 	unittest.main()
